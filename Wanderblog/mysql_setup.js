120dd2515fe6293cda9b2c7257a04c284acc15b2
Biswas Bhandari <bis_sharma2000@yahoo.com>
2015-11-05 11:14:51 +0000

    added queries to sql schema and mysql setip javascript

--- Wanderblog/mysql_setup.js
+++ Wanderblog/mysql_setup.js
@@ -2,9 +2,21 @@
 var connection = mysql.createConnection({
     //Here put credentials for your local sql.
     host     : 'localhost',
-    user     : 'me',
-    password : 'secret',
-    database : 'my_db'
+    user     : 'root',
+    password : 'password',
+    database : 'wanderblog'
 });
 
 connection.connect();
+
+var queryString = 'SELECT * FROM w_user';
+
+connection.query(queryString, function(err, rows, fields) {
+    if (err) throw err;
+
+    for (var i in rows) {
+        console.log('Name: ', rows[i].name);
+    }
+});
+
+connection.end();
--- Wanderblog/schema.sql
+++ Wanderblog/schema.sql
@@ -1,13 +1,20 @@
-﻿drop database if exists dbUsers;
+drop database if exists wanderblog;
 
-create database if not exists dbUsers;
+create database if not exists wanderblog;
 
-use dbUsers;
+use wanderblog;
 
-drop table if exists tblUsers;
+drop table if exists w_user;
 
-create table if not exists tblUsers(
-   userId integer primary key auto_increment,
-   username varchar(100) unique,
-   password varchar(100)
+create table if not exists w_user(
+   id integer primary key auto_increment,
+   type varchar(10) unique,
+   name varchar(50),
+   login_name varchar(20),
+   password varchar(20),
+   country varchar(20),
+   avatar varchar(10)
 )engine=innodb;
+
+insert into w_user
+values(1, "Author", "Firstname Surname", "login_name", "login_password", "country", "picture");
