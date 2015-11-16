f83fae64cae7119717935c412816cbf649fd040f
Biswas Bhandari <bis_sharma2000@yahoo.com>
2015-11-16 14:37:40 +0000

    added some tables - not final

--- schema.sql
+++ schema.sql
@@ -1,20 +1,54 @@
+/* delete the database if it already exists and create a new one*/
 drop database if exists wanderblog;
-
-create database if not exists wanderblog;
+create database wanderblog;
 
 use wanderblog;
 
-drop table if exists w_user;
-
-create table if not exists w_user(
-   id integer primary key auto_increment,
-   type varchar(10) unique,
-   name varchar(50),
-   login_name varchar(20),
-   password varchar(20),
-   country varchar(20),
-   avatar varchar(10)
+drop table if exists user;
+CREATE TABLE user(
+    id INTEGER PRIMARY KEY auto_increment,
+    type VARCHAR(10) NOT NULL,
+    name VARCHAR(50) NOT NULL,
+    login_name VARCHAR(16) NOT NULL,
+    password VARCHAR(20) NOT NULL,
+    country VARCHAR(20) NOT NULL,
+    description TINYTEXT(20) NULL,
+    avatar VARCHAR(10) NULL,
+    CONSTRAINT chk_Type CHECK (type IN ('reader','admin', 'author'))
 )engine=innodb;
 
-insert into w_user
-values(1, "Author", "Firstname Surname", "login_name", "login_password", "country", "picture");
+DROP TABLE IF EXISTS adventure;
+CREATE TABLE adventure(
+    id INTEGER PRIMARY KEY auto_increment,
+    title VARCHAR(30) NOT NULL,
+    location VARCHAR(50) NOT NULL,
+    visit_date DATE NOT NULL,
+    post_date DATETIME(6) NOT NULL,
+    user_id INTEGER NOT NULL,
+    FOREIGN KEY (user_id) REFERENCES user(id) ON DELETE CASCADE
+)engine=innodb;
+
+DROP TABLE IF EXISTS tag;
+CREATE TABLE tag(
+    id INTEGER PRIMARY KEY auto_increment,
+    name VARCHAR(20) NOT NULL
+)engine=innodb;
+
+DROP TABLE IF EXISTS adventure_tag;
+CREATE TABLE adventure_tag(
+    adventure_id INTEGER NOT NULL,
+    tag_id INTEGER NOT NULL,
+    FOREIGN KEY (adventure_id) REFERENCES adventure(id) ON DELETE CASCADE,
+    FOREIGN KEY (tag_id) REFERENCES tag(id) ON DELETE CASCADE
+)engine=innodb;
+
+DROP TABLE IF EXISTS comment;
+CREATE TABLE comment(
+    id INTEGER PRIMARY KEY auto_increment,
+    post_date DATETIME(6) NOT NULL,
+    content TEXT NOT NULL,
+    user_id INTEGER NOT NULL,
+    adventure_id INTEGER NOT NULL,
+    FOREIGN KEY (adventure_id) REFERENCES adventure(id) ON DELETE CASCADE,
+    FOREIGN KEY (user_id) REFERENCES user(id) ON DELETE CASCADE
+)engine=innodb;
