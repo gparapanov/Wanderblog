cdde2c009d4eb5071bb44d79cede391e931127fa
Adrian Grzywaczewski <adrian.grzywaczewski@gmail.com>
2015-11-19 15:15:55 +0000

    azure fix

--- server.js
+++ server.js
@@ -43,7 +43,7 @@ app.use(session({
 }));
 
 // all environments
-app.set('port', process.env.PORT || 1337);
+app.set('port', process.env.PORT || 8080);
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'jade');
 app.use(express.favicon());
