71d0499d09c71ff13b34eb1fc4e73fdc4498d1ed
Adrian Grzywaczewski <adrian.grzywaczewski@gmail.com>
2015-11-24 23:16:06 +0000

    Adventures content works now

--- routes/adventures.js
+++ routes/adventures.js
@@ -6,7 +6,7 @@ module.exports = function(app){
         // Description of adventure needs to be added,
         // query for amount of comments
         // pictures/avatar
-        connection.query("SELECT u.login_name, u.avatar, u.id, a.visit_date, a.title, a.location, a.user_id FROM users as u INNER JOIN adventure AS a ON u.id = a.user_id", function (err, rows) {
+        connection.query("SELECT u.login_name, u.avatar, u.id, a.content_text, a.visit_date, a.title, a.location, a.user_id FROM users as u INNER JOIN adventure AS a ON u.id = a.user_id", function (err, rows) {
             //catch connection error
             if (err) {
                 console.error("Error connecting: " + err.stack);
@@ -16,6 +16,7 @@ module.exports = function(app){
             for (i = 0; i < rows.length; i++) {
                 var adv = {
                     title: rows[i].title,
+                    content: rows[i].content_text,
                     login_name: rows[i].login_name,
                     avatar: rows[i].avatar,
                     location: rows[i].location,
