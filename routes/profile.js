702969a0a3c6bca2af504d533e0be51e6dbc22b0
Adrian Grzywaczewski <adrian.grzywaczewski@gmail.com>
2015-11-26 20:32:48 +0000

    profile routes and profile page work

--- routes/profile.js
+++ routes/profile.js
@@ -1,53 +1,91 @@
-var db = require('../server.js');
-var connection = db();
+
 var bodyParser = require('body-parser');
+var mysql = require('mysql');
+var pool  = mysql.createPool({
+    host: 'localhost',
+    user: 'root',
+    password: '',
+    database: 'wanderblog',
+});
+
+function queryRows(col, table) {
+    return function(cb) {
+        var rows = [];
+        db.execute('SELECT ' + col + ' FROM ' + table)
+            .on('row', function(r) {
+                rows.push(r)
+            })
+            .on('result', function() {
+                cb(rows);
+            });
+    }
+}
 module.exports = function (app) {
     app.get('/profile', function (req, res) {
         res.render('profile.jade');
     });
 
-
-    //A better way of finding the number of adventures must be implemented (asynchronus // callbacks?)
     app.get('/profile/:id', function (req, res) {
         var id = req.params.id;
         var idString = String(id)
         console.log(idString);
-        var adventures = [];
-        var sqlQuery = 'SELECT * FROM users WHERE id =? LIMIT 1';
-        'SELECT title,country FROM adventure where user_id=?';
-        connection.query("SELECT * FROM users WHERE id =? LIMIT 1", idString, function (err, rows) {
-            //Calculating the number of adventures
-            if (rows.length != 0) {
-                var adventures = [rows.length];
-                for (adv in adventures) {
-                    adventures.push(rows.title);
+        var ifHasPosts = "SELECT u.name, u.id, u.description, u.type, u.avatar, u.email, u.login_name, u.country, a.title FROM users as u INNER JOIN adventure AS a ON u.id = a.user_id WHERE u.id =?;";
+        var userCredentials = "SELECT * from users WHERE id = ?;";
+        var twoQueries =ifHasPosts+userCredentials;
+        console.log(twoQueries);
+
+        pool.getConnection(function(err, connection) {
+            // Use the connection
+            connection.query( ifHasPosts,idString, function(err, rows) {
+                console.log(rows);
+                var adventures = [];
+                if (rows.length != 0) {
+                    for (i = 0; i < rows.length; i++) {
+                        //console.log(rows[i].title)
+                        if (rows[i].title != null) {
+                            adventures.push(rows[i].title);
+                        }
+                    }
+                    console.log(adventures);
+                    //return rows;
+                    res.render('profile.jade', {
+                        //user credentials
+                        type: rows[0].type,
+                        name: rows[0].name,
+                        login_name: rows[0].login_name,
+                        email: rows[0].email,
+                        description: rows[0].description,
+                        country: rows[0].country,
+                        avatar: rows[0].avatar,
+                        //user posts
+                        adventures: adventures,
+                        title: 'Profile',
+                        year: new Date().getFullYear(),
+                        message: 'Profile page'
+                    });
                 }
-                var userID = rows[0].id;
-                res.render('profile.jade', {
-                    //user credentials
-                    type: rows[0].type,
-                    name: rows[0].name,
-                    login_name: rows[0].login_name,
-                    email: rows[0].email,
-                    description: rows[0].description,
-                    country: rows[0].country,
-                    avatar: rows[0].avatar,
-                    //user posts
-                    adventures: adventures,
-                    title: 'Profile',
-                    year: new Date().getFullYear(),
-                    message: 'Profile page'
-
+                else {
+                    connection.query( userCredentials,idString, function(err, rows) {
+                        res.render('profile.jade', {
+                            //user credentials
+                            type: rows[0].type,
+                            name: rows[0].name,
+                            login_name: rows[0].login_name,
+                            email: rows[0].email,
+                            description: rows[0].description,
+                            country: rows[0].country,
+                            avatar: rows[0].avatar,
+                            //user posts
+                            adventures: adventures,
+                            title: 'Profile',
+                            year: new Date().getFullYear(),
+                            message: 'Profile page'
+                        });
                 });
-                adventures = [];
-                connection.end();
-            }
-            else {
-                //if user doesnt exist redirect
-                res.redirect('/login');
-            }
-        })
-
+                    connection.release();
+                }
+            });
+        });
 
     });
 }
