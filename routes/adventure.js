2b7a836b8ee8f9d10bbbf7aad0896f8f3e4870eb
gparapanov <georgi.parapanov@gmail.com>
2015-11-26 22:37:04 +0000

    nothing

--- routes/adventure.js
+++ routes/adventure.js
@@ -1,10 +1,10 @@
 var db = require('../server.js');
 var connection = db();
 module.exports = function (app) {
-    app.get('/adventure', function (req, res) {
+    app.get('/adventure/:id', function (req, res) {
         //app.get('adventures/:id', function( req, res){
         //...This will allow us to load a page wanderblog.bla/adventures/1 <- this will show a post with id 1.
-        var adventureid=1;//req.params.id
+        var adventureid=req.params.id;
         var results=[];
         var queryString="SELECT * FROM adventure";
         var resultsFunc = function () {
@@ -14,6 +14,7 @@ module.exports = function (app) {
                     if (rows.length != 0){
 
                         if(rows[i].id==adventureid){
+                            console.log(rows[i].title);
                             results.push(rows[i].id);
                             results.push(rows[i].title);
                             results.push(rows[i].location);
