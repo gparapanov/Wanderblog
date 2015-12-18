cd1d8b98f60a0c99f90b44315454b6424377bada
Adrian Grzywaczewski <adrian.grzywaczewski@gmail.com>
2015-12-18 20:07:56 +0000

    check final

--- db.js
+++ db.js
@@ -1,9 +1,9 @@
 var mysql = require('mysql');
 var pool = mysql.createPool({
-    host: 'localhost',
-    user: 'root',
-    password: 'password',
-    database: 'wanderblog'
+    host: 'us-cdbr-azure-central-a.cloudapp.net',
+    user: 'bc103542111d8a',
+    password: '5de22dac',
+    database: 'acsm_e6b305d56de31f5'
 });
 
 exports.getConnection = function(callback){
