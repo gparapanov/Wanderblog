709bf77f185aec88f29c606fec9388bf055aafef
Adrian Grzywaczewski <adrian.grzywaczewski@gmail.com>
2015-12-18 17:43:28 +0000

    index page update

--- routes/index.js
+++ routes/index.js
@@ -11,7 +11,7 @@ module.exports = function (app, db) {
     //Login
     app.get('/', function (req, res) {
         db.getConnection(function (err, connection) {
-            connection.query("SELECT adventure_id, SUM(score) as total from rating group by adventure_id order by sum(score) DESC;", function (err, rows) {
+            connection.query("SELECT adventure_id,title, SUM(score) as total from rating,adventure where adventure_id=adventure.id group by adventure_id,title order by sum(score) DESC;", function (err, rows) {
                 //catch connection error
                 if (err) {
                     console.error("Error connecting: " + err.stack);
@@ -21,18 +21,19 @@ module.exports = function (app, db) {
                 for (i = 0; i < 5; i++) {
                     var adv = {
                         adv_id: rows[i].adventure_id,
-                        adv_score: rows[i].total
+                        adv_score: rows[i].total,
+                        adv_title: rows[i].title
                     };
                     top.push(adv);
-
                 }
-                console.log(top);
+
+
                 connection.release();
+                console.log(top);
                 res.render('index.jade', {topAdventures: top, isLoggedIn: req.session.isLoggedIn});
+                //console.log(top);
             });
         });
-
-
     });
 }
 
